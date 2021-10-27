691bf7723a661f08eafe6a0d7f210f26f4bd1233
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-10-27 13:05:01 -0400

    added front end to project, added get request for main html, added middleware function to use js and css

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // set up middleware functions
 // parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
 // parse incoming JSON data
 app.use(express.json());
+// Make static resources readily available
+app.use(express.static('public'));
 
 
 function filterByQuery (query, animalsArray) {
@@ -113,6 +116,10 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
